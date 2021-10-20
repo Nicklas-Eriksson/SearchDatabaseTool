f969278d3fa3f65f6db41afcb004c1b2d3c2786f
mattias vikström <mattias@dragnadrag.se>
2021-10-20 10:26:35 +0200

    Testsuite added

---
+++ SearchDatabaseToolTests/Calculations/FindWordsTests.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SearchDatabaseToolTests.Calculations
+{
+    [TestClass]
+    class FindWordsTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
+
